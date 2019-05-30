148c65f088370636c94eb6a49d59c36bfb7734ac
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-05-30 15:39:32 -0400

    read me

--- src/UserFeed/UserFeed.js
+++ src/UserFeed/UserFeed.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-import UserDetails from '../UserDetails/UserDetails';
 import RecipeSquares from '../RecipeSquares/RecipeSquares';
 import SearchBar from '../SearchBar/SearchBar';
 import './UserFeed.css';
